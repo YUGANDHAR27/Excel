6229883afcbb0e2bf2ba340a39e7568a078e95e9
Yugandhar Palyam <Yugandhar.Palyam@ATMECS.CORP>
2022-04-19 15:14:47 +0530

    Method added

--- src/readExcelData/ReadExcel.java
+++ src/readExcelData/ReadExcel.java
@@ -27,5 +27,25 @@ public class ReadExcel {
 		
 
 	}
+	public static void sample ()throws Exception 
+	{
+		File file=new File("C:\\Users\\yugandhar.palyam\\Documents\\Book1.xlsx");
+		FileInputStream inputStream=new FileInputStream(file);
+		XSSFWorkbook wb=new XSSFWorkbook(inputStream);
+		XSSFSheet sheet=wb.getSheetAt(0);
+		//String data=sheet.getRow(1).getCell(3).getStringCellValue();
+		//System.out.println("data is " + data);
+		int rowcount=sheet.getLastRowNum();
+		System.out.println("total row count " + rowcount);
+		for(int row=0;row<rowcount;row++) {
+			String data=sheet.getRow(row).getCell(0).getStringCellValue();
+			System.out.println("data from row is " + row + "is " + data);
+		}
+		wb.close();
+		
+		
+		
+
+	}
 
 }
